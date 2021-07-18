45d84f44e943d627de7fa0b6e15d595e8a9394ec
cjgreenie <seejarom@gmail.com>
2021-07-17 23:29:05 -0600

    don't need a tasksController

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ import TasksController from "./Controllers/TasksController.js"
 class App {
   // valuesController = new ValuesController();
   listsController = new ListsController();
-  tasksController = new TasksController();
+
 }
 
 window["app"] = new App();
